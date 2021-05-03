ac369186a7b8ed415fd41adaf4c7e3063c794870
Kaikka <kaileeto@gmail.com>
2021-05-03 10:50:50 +0200

    Comments for source of files

--- backend/src/main/java/no/kristiania/backend/PasswordConfig.java
+++ backend/src/main/java/no/kristiania/backend/PasswordConfig.java
@@ -1,9 +1,13 @@
-package no.kristiania;
+package no.kristiania.backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+/*
+This class was taken from https://github.com/arcuri82/testing_security_development_enterprise_systems/blob/master/intro/exercise-solutions/quiz-game/part-11/backend/src/main/java/org/tsdes/intro/exercises/quizgame/backend/PasswordConfig.java
+*/
 
 @Configuration
 public class PasswordConfig {
