fe5fdf93a5513670b828867236e0d6f3808710ce
Kaikka <kaileeto@gmail.com>
2021-05-03 13:06:29 +0200

    reviewservice with methods for getting all reviews sorted by either rating or time of creation

---
+++ backend/src/main/java/no/kristiania/backend/service/ReviewService.java
@@ -0,0 +1,40 @@
+package no.kristiania.backend.service;
+
+import no.kristiania.backend.entity.Review;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ReviewService {
+
+    // TODO
+    //     - Retrieve all reviews for movie, sorted by stars OR creation time of the review
+
+    @Autowired
+    private EntityManager em;
+
+
+    public List<Review> getAllReviewsSortedByAvgRating(long movieId) {
+
+        TypedQuery<Review> query = em.createQuery(
+                "SELECT r FROM Review r WHERE r.reviewId.movieId=:movieId ORDER BY r.rating DESC", Review.class);
+        query.setParameter("movieId", movieId);
+        return query.getResultList();
+    }
+
+    public List<Review> getAllReviewsSortedByTime(long movieId) {
+
+        TypedQuery<Review> query = em.createQuery(
+                "SELECT r FROM Review r WHERE r.reviewId.movieId=:movieId ORDER BY r.dateCreated DESC", Review.class);
+        query.setParameter("movieId", movieId);
+        return query.getResultList();
+    }
+
+    //TODO: delete review?
+}
