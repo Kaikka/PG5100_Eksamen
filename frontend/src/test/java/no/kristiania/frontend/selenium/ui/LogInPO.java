a14f5aedf27411c3cb4d06f92a659db77cca72d0
Kaikka <kaileeto@gmail.com>
2021-05-05 00:16:40 +0200

    LoginPageObject

---
+++ frontend/src/test/java/no/kristiania/frontend/selenium/ui/LogInPO.java
@@ -0,0 +1,28 @@
+package no.kristiania.frontend.selenium.ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LogInPO {
+    private final WebDriver driver;
+
+    public LogInPO(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public void clickSubmit() {
+        WebElement submit = driver.findElement(By.id("submit"));
+        submit.click();
+    }
+
+    public void enterCredentials(String username, String password) {
+        WebElement user = driver.findElement(By.id("username"));
+        WebElement pass = driver.findElement(By.id("password"));
+
+        user.sendKeys(username);
+        pass.sendKeys(password);
+    }
+}
+
