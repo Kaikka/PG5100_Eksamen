eb599e2ea96235752164ba1b05896b1d61429a17
Kaikka <kaileeto@gmail.com>
2021-05-04 14:23:27 +0200

    added link to andrea's repo

--- backend/src/test/java/no/kristiania/backend/service/DefaultDataInitializerServiceTest.java
+++ backend/src/test/java/no/kristiania/backend/service/DefaultDataInitializerServiceTest.java
@@ -1,7 +1,6 @@
 package no.kristiania.backend.service;
 
 import no.kristiania.backend.StubApplication;
-import no.kristiania.backend.entity.Movie;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +8,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = StubApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.NONE)
-
+/*
+Class adapted from https://github.com/arcuri82/testing_security_development_enterprise_systems/blob/master/intro/exercise-solutions/quiz-game/part-11/backend/src/test/java/org/tsdes/intro/exercises/quizgame/backend/service/DefaultDataInitializerServiceTest.java
+*/
 @DirtiesContext(classMode = BEFORE_CLASS)
 public class DefaultDataInitializerServiceTest {
 
@@ -26,10 +25,7 @@ public class DefaultDataInitializerServiceTest {
 
     @Test
     public void testInit(){
-
-        List<Movie> movies = movieService.getAllMovies();
-
-        assertTrue(movies.size() > 0);
+        assertTrue(movieService.getAllMovies().size() > 0);
     }
 
 }
